c6bb250637fab17c015d6e43075b98becd2d34e2
José <jarcovision@cafetera.local>
2022-12-15 21:28:29 -0600

    Base de datos2-1a clase

---
+++ basededatos2/dermaSpa/src/test/java/com/generation/dermaspa/DermaSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.dermaspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermaSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
